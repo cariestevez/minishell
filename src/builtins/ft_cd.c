a20c2878d361a22c629b2b27fbea396575a4ef18
esther <esthers@live.dk>
2023-12-13 20:39:50 +0100

    norminetted cd and env🐇

--- src/builtins/ft_cd.c
+++ src/builtins/ft_cd.c
@@ -1,57 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_cd.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emollebr <emollebr@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/13 20:22:38 by emollebr          #+#    #+#             */
+/*   Updated: 2023/12/13 20:22:40 by emollebr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-//exit status 0 on success
-//exit status 2 on incorrect usage, generally invalid options or missing arguments
-
-char    *get_curpath(char *directory, char **env)
+char	*get_curpath(char *directory, char **env)
 {
-    char    *curpath;
-    
-    if (!directory)
-            return (ft_strdup(ft_getenv("HOME", env)));
-    if (ft_strncmp(directory, "-", 2) == 0)
-        return (ft_printf("%s\n", ft_getenv("OLDPWD", env)), ft_strdup(ft_getenv("OLDPWD", env)));
-    if (ft_strncmp(directory, "~", 1) == 0)
-    {
-        curpath = ft_strjoin(ft_getenv("HOME", env), "/");
-        directory = ft_strjoin(curpath, directory + 1);
-        free(curpath);
-    }
-    if (directory[0] == '/' || directory[0] == '.')
-        curpath = directory;
-    else
-        curpath = get_path(directory, env);
-    return (curpath);
+	char	*curpath;
+
+	if (!directory)
+		return (ft_strdup(ft_getenv("HOME", env)));
+	if (ft_strncmp(directory, "-", 2) == 0)
+	{
+		ft_printf("%s\n", ft_getenv("OLDPWD", env));
+		return (ft_strdup(ft_getenv("OLDPWD", env)));
+	}
+	if (ft_strncmp(directory, "~", 1) == 0)
+	{
+		curpath = ft_strjoin(ft_getenv("HOME", env), "/");
+		directory = ft_strjoin(curpath, directory + 1);
+		free(curpath);
+	}
+	if (directory[0] == '/' || directory[0] == '.')
+		curpath = directory;
+	else
+		curpath = get_path(directory, env);
+	return (curpath);
 }
 
-int    update_envvar(char *name, char *new, char **env)
+int	update_envvar(char *name, char *new, char **env)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (ft_strncmp(env[i], name, ft_strlen(name)) != 0)
-        i++;
-    free(env[i]);
-    name = ft_strjoin(name, "=");
-    env[i] = ft_strjoin(name, new);
-    free(new);
-    return (0);
+	i = 0;
+	while (ft_strncmp(env[i], name, ft_strlen(name)) != 0)
+		i++;
+	free(env[i]);
+	name = ft_strjoin(name, "=");
+	env[i] = ft_strjoin(name, new);
+	free(new);
+	return (0);
 }
 
-int ft_cd(t_shell *shell, t_simple_cmds *cmd)
+int	ft_cd(t_shell *shell, t_simple_cmds *cmd)
 {
-    char    *curpath;
-    char    *old_pwd;
+	char	*curpath;
+	char	*old_pwd;
 
-    curpath = get_curpath(cmd->str[1], shell->env);
-    if (curpath == NULL)
-        return (printf("minishell: cd: %s: No such file or directory\n", cmd->str[1]), 2);
-    old_pwd = getcwd(NULL, 0);
-    update_envvar("OLDPWD", old_pwd, shell->env);
-    if (chdir(curpath) < 0)
-        return (perror("minishell: cd"), 2);
-    //free(curpath);
-    curpath = getcwd(NULL, 0);
-    update_envvar("PWD", curpath, shell->env);
-    return (0);
+	curpath = get_curpath(cmd->str[1], shell->env);
+	if (curpath == NULL)
+	{
+		printf("minishell: cd: %s: No such file or directory\n", cmd->str[1]);
+		return (2);
+	}
+	old_pwd = getcwd(NULL, 0);
+	update_envvar("OLDPWD", old_pwd, shell->env);
+	if (chdir(curpath) < 0)
+		return (perror("minishell: cd"), 2);
+	curpath = getcwd(NULL, 0);
+	update_envvar("PWD", curpath, shell->env);
+	return (0);
 }
--- src/builtins/ft_unset.c
+++ src/builtins/ft_unset.c
@@ -1,33 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_unset.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emollebr <emollebr@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/13 20:23:15 by emollebr          #+#    #+#             */
+/*   Updated: 2023/12/13 20:23:17 by emollebr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 int	ft_unset(t_shell *shell, t_simple_cmds *cmd)
 {
-	int	i;
-	int	j;
 	char	**list;
+	int		i;
+	int		j;
 
 	list = shell->env;
 	i = 1;
 	j = 0;
 	while (cmd->str[i])
 	{
-			j = 0;
-			while (ft_strncmp(cmd->str[i], list[j], ft_strlen(cmd->str[i])) != 0
-				&& list[j] != NULL)
+		j = 0;
+		while (ft_strncmp(cmd->str[i], list[j], ft_strlen(cmd->str[i])) != 0
+			&& list[j] != NULL)
+		{
+			j++;
+		}
+		if (cmd->str[i] != NULL)
+		{
+			free(list[j]);
+			while (list[j] != NULL)
 			{
+				list[j] = list[j + 1];
 				j++;
 			}
-			if (cmd->str[i] != NULL)
-			{
-				free(list[j]);
-				while (list[j] != NULL)
-				{
-					list[j] = list[j + 1];
-					j++;
-				}
-				free(list[j]);
-			}
-			i++;
+			free(list[j]);
+		}
+		i++;
 	}
 	return (0);
 }
